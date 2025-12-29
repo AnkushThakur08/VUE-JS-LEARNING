6c1af92d369942ef81e0ea535ae777db9fc7ca42
Ankush Thakur <atpara99@gmail.com>
2025-12-29 15:59:34 +0530

    Todo App in Vue JS

---
+++ 02todoapp/src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import TodoList from './components/TodoList.vue';
+</script>
+
+<template>
+  <div>
+    <h1 class="text-2xl font-bold text-center my-6">Todo App</h1>
+    <TodoList />
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ 02todoapp/src/components/TodoList.vue
@@ -0,0 +1,120 @@
+<script setup lang="ts">
+import { computed, ref } from "vue";
+import { v4 as uuidv4 } from "uuid";
+import type { ITodo } from "../contracts/Todo";
+
+const todoArr = ref<ITodo[]>([]);
+const selectedFilter = ref<"all" | "pending" | "completed">("all")
+const todoText = ref<string>("");
+console.log("we are here", todoText);
+const handleAddTodo = () => {
+
+  if(!todoText.value.trim()) return
+  todoArr.value.push({
+    id: uuidv4(),
+    todo: todoText.value ?? "",
+    status: false,
+  });
+  todoText.value = "";
+};
+
+const handleTodoStatus = (todo: ITodo) => {
+  todo.status = !todo.status;
+};
+
+const handleDeleteTodo = (id: string) => {
+  todoArr.value = todoArr.value.filter((item) => item.id !== id);
+};
+
+const filteredTodo = computed(() => {
+  if(selectedFilter.value == 'pending'){
+    return todoArr.value.filter((todo) => !todo.status)
+  } else if (selectedFilter.value == 'completed') {
+    return todoArr.value.filter((todo) => todo.status)
+  }
+  
+  return todoArr.value;
+
+})
+
+
+</script>
+
+<template>
+  <div>
+    <div class="flex gap-5 justify-center">
+      <input
+        type="text"
+        name="todoItem"
+        id="todoItem"
+        placeholder="Enter Todo Item"
+        class="border border-slate-200 rounded px-4 py-2"
+        v-model="todoText"
+      />
+      <button
+        type="submit"
+        class="bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-800 cursor-pointer"
+        @click="handleAddTodo"
+      >
+        Add Todo
+      </button>
+
+      <select id="filter" name="filter" v-model="selectedFilter">
+        <option value="all">All</option>
+        <option value="pending">Pending</option>
+        <option value="completed">Completed</option>
+      </select>
+    </div>
+
+    <table
+      class="table-fixed text-center w-full border border-gray-500 border-seperate border-spacing-5 mt-10 p-5"
+    >
+      <thead>
+        <tr>
+          <th class="border border-gray-500">Todo ID</th>
+          <th class="border border-gray-500">Todo Title</th>
+          <th class="border border-gray-500">Status</th>
+          <th class="border border-gray-500">Mark as Done</th>
+          <th class="border border-gray-500">Delete Todo</th>
+        </tr>
+      </thead>
+
+      <tbody>
+        <tr v-for="todo in filteredTodo" :key="todo.id" class="p-6">
+          <td class="border border-gray-500">{{ todo.id }}</td>
+          <td
+            :class="
+              todo.status
+                ? 'border border-gray-500 line-through'
+                : 'border border-gray-500'
+            "
+          >
+            {{ todo.todo }}
+          </td>
+          <td
+            :class="
+              todo.status
+                ? 'border border-gray-500 text-green-600 font-semibold'
+                : 'border border-gray-500 text-yellow-600 font-semibold'
+            "
+          >
+            {{ todo.status ? `Completed` : "Pending" }}
+          </td>
+          <td class="border border-gray-500">
+            <button
+              class="text-white py-0 px-2 rounded cursor-pointer mx-6"
+              @click="handleTodoStatus(todo)"
+            >
+              {{ todo.status ? "❌" : "✅" }}
+            </button>
+          </td>
+          <td>
+            <button class="cursor-pointer" @click="handleDeleteTodo(todo.id)">
+              🗑
+            </button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
